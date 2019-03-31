a6edc8d7f7d57b17aef13f9ea3d82d5f8f8d2e00
dingdj <18950295115@163.com>
2019-03-31 10:21:33 +0800

    add permutation and combination

---
+++ permutation.js
@@ -0,0 +1,28 @@
+var a = [1,2,3,4];
+var result = [];
+
+var clone = function(a) {
+    var b = [];
+    a.forEach(element => {
+        b.push(element);
+    });
+    return b;
+}
+
+var printPermutation = function(a) {
+    if(result.length == 4) {
+        console.log(result);
+        return;
+    }
+
+    for(let i=0; i<a.length; i++) {
+        let v = a[i];
+        result.push(v);
+        let new_a = clone(a);
+        new_a.splice(i, 1);
+        printPermutation(new_a);
+        result.pop();
+    }
+}
+
+printPermutation(a);
